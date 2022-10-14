e2cc4a7331532d11ea6377fdf78126b4342a2fdd
sanchitha1336 <sanchithashimoga@gmail.com>
2022-10-14 20:47:15 +0530

    skils folder

---
+++ src/Components/Skils/index.tsx
@@ -0,0 +1,22 @@
+import { HeaderNav, HeaderNavbar, NavBrand, NavContainer, NavLink } from "./navbar.style";
+import React from "react";
+
+function Skils() {
+  return (
+    <>
+      <HeaderNavbar>
+        <NavContainer>
+        <HeaderNav>
+          <NavBrand>Portfolio</NavBrand>
+        </HeaderNav>
+        <HeaderNav>
+          <NavLink href="#home">Home</NavLink>
+          <NavLink href="#features">Features</NavLink>
+          <NavLink href="#pricing">Pricing</NavLink>
+        </HeaderNav>
+        </NavContainer>
+      </HeaderNavbar>
+    </>
+  );
+}
+export default Skils;
