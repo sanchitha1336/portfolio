8f9697f60381207610b39425141001493834787e
sanchitha1336 <66420123+sanchitha1336@users.noreply.github.com>
2022-10-14 19:53:28 +0530

    creating portfolio set up structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Topbar from "./Components/Header/Topbar";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+        <Topbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/navbar.style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { Navbar, Nav ,Container} from "react-bootstrap";
+
+export const HeaderNav = styled(Nav)`
+`;
+export const HeaderNavbar = styled(Navbar)`
+background-image: linear-gradient(#cc2b5e,#753a88);
+`;
+export const NavLink = styled(Nav.Link)`
+color: aliceblue;
+`;
+export const NavBrand = styled(Navbar.Brand)`
+color: aliceblue;
+`;
+export const NavContainer=styled(Container)``;
